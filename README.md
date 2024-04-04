0bb0e9407b82b57ae6d3b1551ada9604d9205a6e
Urvish <urvishp45003@gmail.com>
2024-04-04 21:39:37 +0530

    completed the initial start

---
+++ README.md
@@ -0,0 +1,7 @@
+# Idea
+
+This is my way of taking quick notes on the go. I have a lot of ideas and thoughts that I want to jot down, but I don't want to open a new file every time I want to write something down. So, I created this script to help me write down my thoughts and ideas quickly.
+
+# How to use
+
+## this is still a work in progress
--- idea.sh
+++ idea.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
 # this is for testing purposes only.
-DEFAULT_PATH="$HOME/Desktop/ideas/tests/ideas"
+DEFAULT_PATH="$HOME/Desktop/ideas/test/ideas"
+
+# get default path
 
 # check if the path exists
 is_path_exists() {
-	local file_path="$1"
-	if [ -e "$file_path" ]; then
-		echo "The path exists."
-		cd "$file_path"
+	local path="$1"
+
+	if [ -d "$path" ]; then
 		return 0
 	else
-		echo nnnnnnnnnnnnnnnnooooooooooooooooo
 		return 1
 	fi
 }
 
-is_path_exists "$DEFAULT_PATH"
+
+
+if is_path_exists "$DEFAULT_PATH"; then
+	nvim "$DEFAULT_PATH/idea.md"
+else
+	read -p "Path does not exist. Do you want to create it? [y/n]: " response
+    if [ "$response" == "y" ]; then
+        read -p "Enter the path: " path
+        if is_path_exists $path; then
+            nvim $path/idea.md
+        else
+            echo "Path does not exist. Exiting..."
+            exit 1
+        fi
+    else
+        echo "Exiting..."
+        exit 1
+fi
+
